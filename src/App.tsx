9e53def73b76cbe1950ee98e9a12fcff44735ecf
Alan Francisco Mora <alanfmorag@gmail.com>
2023-07-03 14:41:55 -0600

    :safety_vest: Layout ready

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import SnippetEditor from "./components/SnippetEditor";
 
 export default function App() {
   return (
-    <div className="bg-neutral-950 h-screen text-white">
-      <SnippetForm />
-      <SnippetList />
-      <SnippetEditor />
+    <div className="h-screen text-white grid grid-cols-12">
+      <div className="col-span-3 bg-zinc-950">
+        <SnippetForm />
+        <SnippetList />
+      </div>
+
+      <div className="col-span-9 bg-neutral-950 flex justify-center items-center">
+        <SnippetEditor />
+      </div>
     </div>
   );
 }
